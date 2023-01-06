ba6c1a58e2e8c6d5a3a4668154f80f9893babf3e
akursus <ak_ursus@list.ru>
2023-01-06 17:33:43 +0300

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-
+        EmployeeBook employeeBook = new EmployeeBook();
+        employeeBook.addEmployee(new Employee("Куинджи", "Архип", "Иванович", "3", 83239));
+        employeeBook.printAllEmployee();
+        employeeBook.printSumSalary();
+        System.out.println("Минимальная зарплата у: " +employeeBook.getEmployeeWithMinSalary());
+        System.out.println("Максимальная зарплата у: " +employeeBook.getEmployeeWithMaxSalary());
+        employeeBook.initialsEmployee();
     }
 }
